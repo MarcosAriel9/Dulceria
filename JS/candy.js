e6051c79c1be11916152cb130df9fe0655bb958f
MarcosAriel9 <20203tn043@utez.edu.mx>
2021-06-17 21:32:51 -0500

    primer commit v1.0

--- JS/candy.js
+++ JS/candy.js
@@ -3,11 +3,11 @@ const validar = () => {
         let user = document.getElementById("UsuarioLogin").value
         let pass = document.getElementById("PassLogin").value
         if (user == 'admin') {
-                var win = window.open("../screens/adminInicio.html", '_blank')
+                var win = window.open("../Dulceria/screens/adminInicio.html", '_blank')
                 win.focus()
                 alert(`Bienvenido ${user}, usuario y contraseña correctos`)
         } else if (user == 'cajero'){
-                var win2 = window.open("../screens/cajeroInicio.html", '_blank')
+                var win2 = window.open("../Dulceria/screens/cajeroInicio.html", '_blank')
                 win.focus()
                 alert(`Bienvenido ${user}, usuario y contraseña correctos`)
         }else{
